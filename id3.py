9eba14457c7cc9b211db3649416e6fc5df743d05
debuggingMyBrain <Jacob@Jacob-PC.(none)>
2015-02-25 19:46:33 -0600

    Added id3 size calculation

--- id3.py
+++ id3.py
@@ -8,7 +8,21 @@ file_epub = r"C:\Users\Mike\Desktop\Emma.epub"
 def hex_to_num(to_convert):
     return int(binascii.hexlify(to_convert),16)
 
+def calc_tag_size(list_of_size_bytes):
+    NUM_OF_RETAINED_BITS = 7 #only want the lowest 7 bits as we have to ignore the MSb
+
+    binary_string = ""
+
+    for byte in reversed(list_of_size_bytes): #Need to reverse the list to be able to concat in a consistent way
+        for bit_index in range(NUM_OF_RETAINED_BITS):
+            recreated_bit = (ord(byte) >> bit_index) & 1 #ord because the byte was read as a utf-8 char
+            binary_string = str(recreated_bit) + binary_string
+
+    return int(binary_string, 2)
+
 count = 0
+
+
 
 #purpose = "read"
 purpose = "do"
@@ -21,7 +35,9 @@ with open(file_mp3,"rb") as f:
     print("Version: " + str(id3_major_version) + "." + str(id3_revision_number))
     id3_flags = f.read(1)
     print("Flags: " + str(id3_flags))
-    id3_size = hex_to_num(f.read(4))
+    NUM_OF_SIZE_BYTES = 4
+    id3_size_bytes_list = [f.read(1) for byte in range(NUM_OF_SIZE_BYTES)] #want to read one byte at a time
+    id3_size = calc_tag_size(id3_size_bytes_list)
     print("Size: " + str(id3_size))
 
     if(purpose=="read"):
